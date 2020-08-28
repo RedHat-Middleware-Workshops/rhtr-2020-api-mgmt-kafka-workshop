ce91cf30792309ed622b01268fd99a48b3eafe3a
Evan Shortiss <evanshortiss@gmail.com>
2020-08-28 20:11:34 +0100

    chore: formatting

--- services/iot-graphql-api/src/config.ts
+++ services/iot-graphql-api/src/config.ts
@@ -1,7 +1,11 @@
 import { get } from 'env-var'
 
 const config = {
-  MONGO_CONNECTION_STRING: get('MONGO_CONNECTION_STRING').default('mongodb://rhtr-admin:changethistosomethingelse@mongodb.city-of-losangeles.svc:27017/').asUrlString(),
+  MONGO_CONNECTION_STRING: get('MONGO_CONNECTION_STRING')
+    .default(
+      'mongodb://rhtr-admin:changethistosomethingelse@mongodb.city-of-losangeles.svc:27017/'
+    )
+    .asUrlString(),
   HTTP_PORT: get('HTTP_PORT').default('8080').asPortNumber(),
   HTTP_RESPONSE_DELAY: get('HTTP_RESPONSE_DELAY').default('0').asIntPositive()
 }
