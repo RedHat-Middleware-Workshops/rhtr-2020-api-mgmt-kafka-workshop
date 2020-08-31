7d9304f9302cde8f8814c1338d82251fc090571a
Evan Shortiss <evanshortiss@gmail.com>
2020-08-31 10:44:54 +0100

    fix: homepage image footer/credit

--- services/sensor-management-ui/src/views/HomeView.tsx
+++ services/sensor-management-ui/src/views/HomeView.tsx
@@ -4,13 +4,18 @@ import LosAngeles from '../images/martin-adams-los-angeles-unsplash.jpg';
 const HomeView: React.FC = () => {
   return (
     <div className="fade-in">
-      <div className="flex justify-center px-8">
+      <div className="flex justify-center px-8 relative">
         <img
           className="rounded-b-lg"
           style={{ maxHeight: '40vh' }}
           src={LosAngeles}
           alt="Downtown Los Angeles"
         />
+        <span className="text-white text-opacity-50 text-xs absolute bottom-0 pb-1">
+          <a href="https://unsplash.com/@martinadams?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            Photo by Martin Adams on Unsplash
+          </a>
+        </span>
       </div>
       <div className="container mx-auto pt-8">
         <div className="flex mb-4">
@@ -25,19 +30,6 @@ const HomeView: React.FC = () => {
         </div>
         <hr />
       </div>
-
-      <footer className="flex items-center justify-center h-10 bg-blue-900 text-center">
-        <span className="text-white text-sm">
-          Photo by{' '}
-          <a href="https://unsplash.com/@martinadams?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-            Martin Adams
-          </a>{' '}
-          on{' '}
-          <a href="https://unsplash.com/s/photos/los-angeles?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-            Unsplash
-          </a>
-        </span>
-      </footer>
     </div>
   );
 };
