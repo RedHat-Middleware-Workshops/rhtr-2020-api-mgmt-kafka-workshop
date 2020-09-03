2839d2fcc6c75c26a69d44883a3d63a9d0802ebf
Evan Shortiss <evanshortiss@gmail.com>
2020-09-03 17:39:00 +0100

    fix: configs for monogdb

--- services/iot-graphql-api/src/config.ts
+++ services/iot-graphql-api/src/config.ts
@@ -2,9 +2,7 @@ import { get } from 'env-var'
 
 const config = {
   MONGO_CONNECTION_STRING: get('MONGO_CONNECTION_STRING')
-    .default(
-      'mongodb://rhtr-admin:changethistosomethingelse@mongodb.city-of-losangeles.svc:27017/'
-    )
+    .default('mongodb://mongoadmin@mongoadmin@city-mongodb:27017/city-info')
     .asUrlString(),
   HTTP_PORT: get('HTTP_PORT').default('8080').asPortNumber(),
   HTTP_RESPONSE_DELAY: get('HTTP_RESPONSE_DELAY').default('0').asIntPositive(),
