0b296abe15de03470d13b66ce96b030b99029ee0
Evan Shortiss <evanshortiss@gmail.com>
2020-09-18 15:59:07 +0100

    feat: add event tables

--- services/postgresql/rhtr-setup.sql
+++ services/postgresql/rhtr-setup.sql
@@ -14,5 +14,20 @@ CREATE TABLE meter (
    longitude real NOT NULL
 );
 
+CREATE TABLE meter_update (
+   id serial PRIMARY KEY,
+   meter_id text NOT NULL references meter(id),
+   timestamp TIMESTAMP NOT NULL,
+   status_text text NOT NULL
+);
+
+CREATE TABLE junction_update (
+   id serial PRIMARY KEY,
+   junction_id text NOT NULL references junction(id),
+   timestamp TIMESTAMP NOT NULL,
+   count_ns int NOT NULL,
+   count_ew int NOT NULL
+);
+
 COPY junction(id,name,latitude,longitude) FROM '/docker-entrypoint-initdb.d/junction_info.csv' DELIMITER ',' CSV HEADER;
 COPY meter(id,address,latitude,longitude) FROM '/docker-entrypoint-initdb.d/meter_info.csv' DELIMITER ',' CSV HEADER;
