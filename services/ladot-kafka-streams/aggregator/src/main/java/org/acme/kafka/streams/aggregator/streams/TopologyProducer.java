11e66a0999901891735735623d80ce5e48d6e9bf
Evan Shortiss <evanshortiss@gmail.com>
2020-10-01 12:00:04 +0100

    fix: serdes issues with kafka streams

--- services/ladot-kafka-streams/aggregator/src/main/java/org/acme/kafka/streams/aggregator/streams/TopologyProducer.java
+++ services/ladot-kafka-streams/aggregator/src/main/java/org/acme/kafka/streams/aggregator/streams/TopologyProducer.java
@@ -7,21 +7,15 @@ import org.acme.kafka.streams.aggregator.model.MeterUpdate;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
-import java.time.Duration;
 import java.util.Collections;
 
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.GlobalKTable;
-import org.apache.kafka.streams.kstream.Grouped;
-import org.apache.kafka.streams.kstream.JoinWindows;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.Window;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 
@@ -35,7 +29,8 @@ public class TopologyProducer {
     private static final Logger LOG = Logger.getLogger(TopologyProducer.class);
     static final String METER_INFO_TOPIC = "city-info.updates.public.meter";
     static final String METER_UPDATE_TOPIC = "city-info.updates.public.meter_update";
-    static final String METER_DATA_AGGREGATED_TOPIC = "meter-event-aggregated";
+    static final String METER_DATA_AGGREGATED_TOPIC = "hydrated-meter-events";
+    static final String METER_KTABLE_TOPIC = "meter-info-ktable";
 
     @Produces
     public Topology buildTopology() {
@@ -55,26 +50,28 @@ public class TopologyProducer {
         // KTable<String, MeterInfo> meters = builder.table(METER_INFO_TOPIC, Consumed.with(Serdes.String(), meterInfoSerde));
         LOG.info("created KTable for topic: " + METER_INFO_TOPIC);
 
+        // Generate a simplified key value mapped table of meter info, i.e: "meter_id:address,lat,lng" etc
         KTable<String, MeterInfo> meters = builder.stream(METER_INFO_TOPIC, Consumed.with(Serdes.String(), meterInfoSerde))
             .map((k, v) -> {
-                LOG.info("mapping meter info");
+                LOG.info("mapping meter info for key: " + v.id);
                 return KeyValue.pair(v.id, v);
             })
-            .through("test", Produced.with(Serdes.String(), meterInfoSerde))
+            .through(METER_KTABLE_TOPIC, Produced.with(Serdes.String(), meterInfoSerde))
             .toTable();
 
         builder.stream(METER_UPDATE_TOPIC, Consumed.with(Serdes.String(), meterUpdateSerde))
                 .map((k, v) -> {
-                    LOG.info("mapping meter update");
+                    LOG.info("mapping meter update for meter:" + v.id);
                     return KeyValue.pair(v.meter_id, v);
                 })
                 .peek((k, v) -> {
                     LOG.info("Key: " + k + ", Value: " + v.toString());
                 })
-                .leftJoin(
+                .join(
                     meters,
                     (update, info) -> {
                         LOG.info("Joining meter_update with ID " + update.meter_id + " to meter info with ID " + info.id);
+
                         return new Aggregation(
                             info.address,
                             update.meter_id,
@@ -83,11 +80,9 @@ public class TopologyProducer {
                             info.latitude,
                             info.longitude
                         );
-                    }
+                    },
+                    Joined.with(Serdes.String(), meterUpdateSerde, meterInfoSerde)
                 )
-                .map((k, v) -> {
-                    return KeyValue.pair(v.meter_id, v);
-                })
                 .to(METER_DATA_AGGREGATED_TOPIC, Produced.with(Serdes.String(), aggregationSerde));
 
         return builder.build();
