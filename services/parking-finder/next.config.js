57b8a030b5701a6eab8f1712832a859ba046ac0f
Evan Shortiss <evanshortiss@gmail.com>
2022-06-07 10:37:13 -0700

    fix: environment variable for api key

--- services/parking-finder/next.config.js
+++ services/parking-finder/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    REACT_APP_MAPS_API_KEY: process.env.REACT_APP_MAPS_API_KEY,
+  }
 }
 
 module.exports = nextConfig
